99e2f096d6b485421b31c9a9378873c6c85f79b1
RohanN1818 <rohannrohann90@gmail.com>
2026-02-22 02:13:04 +0530

    Fix apiKey formatting in firebaseConfig
    Corrected formatting of the apiKey in firebaseConfig.

--- dashboard.js
+++ dashboard.js
@@ -12,7 +12,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZeoHVMcxsE9m.JHUbqoepFPmWzhihRPy8",
+  apiKey: "AIzaSyBZeoHVMcxsE9mJHUbqoepFPmWzhihRPy8",
   authDomain: "rohan-styles.firebaseapp.com",
   projectId: "rohan-styles",
   storageBucket: "rohan-styles.firebasestorage.app",
