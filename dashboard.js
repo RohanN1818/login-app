afbbcc8741f4e7ea1bc79ec63919163c8b391847
RohanN1818 <rohannrohann90@gmail.com>
2026-02-22 01:44:09 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -12,12 +12,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
 const firebaseConfig = {
-apiKey: "AIzaSyBZeoHVMcxsE9mJHUbqoepFPmWzhihRPy8",
-authDomain: "rohan-styles.firebaseapp.com",
-projectId: "rohan-styles",
-storageBucket: "rohan-styles.firebasestorage.app".
-messagingSender ld: "610424653799",
-appId: "1:610424653799:web:3e97d73ef97c0f4db6df42"
+  apiKey: "AIzaSyBZeoHVMcxsE9m.JHUbqoepFPmWzhihRPy8",
+  authDomain: "rohan-styles.firebaseapp.com",
+  projectId: "rohan-styles",
+  storageBucket: "rohan-styles.firebasestorage.app",
+  messagingSenderId: "610424653799",
+  appId: "1:610424653799:web:3e97d73ef97c0f4db6df42"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -25,14 +25,16 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 onAuthStateChanged(auth, async (user) => {
-  if(user){
+  if (user) {
+
     document.getElementById("user-email").innerText = user.email;
     document.getElementById("user-name").innerText = user.displayName;
     document.getElementById("profile-pic").src = user.photoURL;
 
     const docSnap = await getDoc(doc(db, "users", user.uid));
-    if(docSnap.exists()){
-      if(docSnap.data().role === "admin"){
+
+    if (docSnap.exists()) {
+      if (docSnap.data().role === "admin") {
         document.getElementById("admin-panel").style.display = "block";
       }
     }
@@ -42,21 +44,22 @@ onAuthStateChanged(auth, async (user) => {
   }
 });
 
-window.logout = async function(){
+window.logout = async function () {
   await signOut(auth);
   window.location.href = "index.html";
 };
 
-// THEME TOGGLE
+// THEME
 const toggleBtn = document.getElementById("theme-toggle");
 
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
-  localStorage.setItem("theme",
+  localStorage.setItem(
+    "theme",
     document.body.classList.contains("dark-mode") ? "dark" : "light"
   );
 });
 
-if(localStorage.getItem("theme") === "dark"){
+if (localStorage.getItem("theme") === "dark") {
   document.body.classList.add("dark-mode");
 }
